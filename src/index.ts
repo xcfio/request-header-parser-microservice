6bdea8e4d289b488c32d2313ced70a99da7c2584
SoftwareX Plus <119097812+xcfio@users.noreply.github.com>
2024-06-19 18:31:11 +0600

    add logic

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Something Cool")
+import express from "express"
+import cors from "cors"
+
+const app = express().use(cors({ optionsSuccessStatus: 200 }))
+
+app.get("/", (_req, res) => res.send("Hello World"))
+app.get("/api/whoami", (req, res) => {
+    res.json({
+        ipaddress: req.ip,
+        software: req.headers["user-agent"],
+        language: req.headers["accept-language"]
+    })
+})
+
+app.listen(3000, () => console.log("Server is running"))
